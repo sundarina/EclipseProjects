6a198c233ace3bf52e206a5e4d4ad8d8f6f4d0cd
Sundarina <sundarina@ukr.net>
2016-10-31 12:51:41 +0200

    Посчитать пятую степень матрицы с помощью метода, который перемножает 2 матрицы.
    2 варианта для умножения разных матриц и для квадратной матрицы
    
    вариант для сдачи дз

--- MatrixMultiplection.java
+++ MatrixMultiplection.java
@@ -1,5 +1,10 @@
+
 //Розумняк Дарья
 //Домашка/Методы/Умножение матрицы на саму себя
+
+
+// Посчитать пятую степень матрицы с помощью метода, который перемножает 2 матрицы. 
+//2 варианта для умножения разных матриц и для квадратной матрицы
 
 import java.util.Scanner;
 
@@ -31,7 +36,9 @@ public class MatrixMultiplection {
 		int[][] res = new int[a.length][b[0].length];
 
 		for (int i = 0; i < a.length; i++) {
+
 			for (int j = 0; j < b[0].length; j++) {
+				res[i][j] = 0;
 				for (int k = 0; k < b.length; k++) {
 					res[i][j] += a[i][k] * b[k][j];
 				}
@@ -45,55 +52,73 @@ public class MatrixMultiplection {
 		int m = 0;
 		int n = 0;
 		int l = 0;
-		boolean exit = false;
+		boolean exid = false;
 
 		do {
-			
-			System.out.println("Введите количество строк первой матрицы : ");
-			m = scanner.nextInt();
-			System.out.println("Введите количество столбцов первой матрицы: ");
-			n = scanner.nextInt();
 			System.out.println(
-					"Введите количество столбцов второй матрицы (количество ее строк совпадает с количеством столбцов первой матрицы) : ");
-			l = scanner.nextInt();
+					"Вы хотите умножить две разных матрицы (1) или найти заданную степень квадратной матрицы(2)? Введите ответ: ");
+			int userSelection = scanner.nextInt();
 
-			System.out.println("Степень умножение матрицы: ");
-			int count = scanner.nextInt();
-		
-			int[][] a = new int[m][n];
-			int[][] b = new int[n][l];
+			if (userSelection == 1) {
+				System.out.println("Введите количество строк первой матрицы : ");
+				m = scanner.nextInt();
+				System.out.println("Введите количество столбцов первой матрицы: ");
+				n = scanner.nextInt();
+				System.out.println(
+						"Введите количество столбцов второй матрицы (количество ее строк совпадает с количеством столбцов первой матрицы) : ");
+				l = scanner.nextInt();
 
-			a = randArray(a);
-			System.out.println("Ваш массив #1: ");
-			output(a);
-			System.out.println();
-			b = randArray(b);
-			System.out.println("Ваш массив #2: ");
-			output(b);
+				int[][] a = new int[m][n];
+				int[][] b = new int[n][l];
 
-			int[][] res = new int[m][l];
-			res = multiplection(a, b);
+				a = randArray(a);
+				System.out.println("Вашф матрица #1: ");
+				output(a);
+				System.out.println();
+				b = randArray(b);
+				System.out.println("Ваша матрица #2: ");
+				output(b);
 
-			System.out.println();
-			System.out.println("Ваша умноженная матрица: ");
-			output(res);
+				int[][] res = new int[m][l];
+				res = multiplection(a, b);
 
-			if (m == n && n == l) {
-				for (int i = 0; i < count; i++) {
-					res = multiplection(res, res);
+				System.out.println();
+				System.out.println("Ваша умноженная матрица: ");
+				output(res);
+				break;
+
+			} else if (userSelection == 2) {
+
+				System.out.println("Введите сторону квадратной матрицы : ");
+				m = scanner.nextInt();
+
+				System.out.println("Степень умножение матрицы: ");
+				int count = scanner.nextInt();
+
+				int[][] a = new int[m][m];
+
+				a = randArray(a);
+				System.out.println("Вашa матрица: ");
+				output(a);
+				System.out.println();
+
+				int[][] res = new int[m][m];
+				res = multiplection(a, a);
+
+				for (int i = 0; i < count - 2; i++) {
+					res = multiplection(a, res);
 				}
 
 				System.out.println();
 				System.out.println("Ваша умноженная матрица в " + count + " степени: ");
 				output(res);
-			} else {
-				System.out.println("Ваша матрица не квадратная, если хотите получить ее степень -  повторите ввод");
-				exit = true;
-			}
+				break;
+			} else
+				System.out.println("Неверный ввод, повторите попытку");
+			System.out.println();
+			exid = true;
 
-		} while (exit);
-
+		} while (exid);
 		scanner.close();
 	}
-
 }
