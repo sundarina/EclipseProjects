76e5c033f0257f0f95bb4c2ff0dba00fa3825fac
Sundarina <sundarina@ukr.net>
2016-11-04 18:31:17 +0200

    готово 3 задания

--- RecursionLesson.java
+++ RecursionLesson.java
@@ -1,38 +1,28 @@
-import java.util.Scanner;
+//Розумняк Дарья
+//Рекурсия
+
 import java.util.*;
 
 public class RecursionLesson {
 
+	static void printArray(int arr[], int n) {
+		if (n == 0)
+			return;
+		System.out.print(arr[--n] + " ");
+		printArray(arr, n);
+	}
+
 	/*
 	 * 1. Создать массив на N элементов со случайными элементами. Циклы не
 	 * использовать.
-	 * 
-	 * 2. Посчитать факториал числа, используя рекурсивные алгоритмы.
-	 * 
-	 * 3. Найти число Фибоначчи под номером N, используя рекурсию.
-	 * 
-	 * 4. Посчитать детерминант матрицы, используя рекурсию.
-	 * 
-	 * 5. Отсортировать массив, используя рекурсию.
-	 * 
-	 * 6. Решить задачу с помощью рекурсии. Дана матрица нулевых элементов. В
-	 * любом месте матрицы ставится одно значение 1. Нужно посчитать за сколько
-	 * ходов единица «захватит мир», если каждый ход область ее владений
-	 * расширяется на соседние элементы.
 	 */
 
 	static void randomArray(int arr[], int n) {
 		if (n == 0)
 			return;
-		int i = (int)(Math.random() * 99);
+		int i = (int) (Math.random() * 99);
 		arr[--n] = i;
 		randomArray(arr, n);
-	}
-	
-	static void printArray(int arr[], int n) {
-		if ( n == 0) return;
-		System.out.print(arr[--n] + " ");
-		printArray(arr, n);		
 	}
 
 	/*
@@ -47,26 +37,52 @@ public class RecursionLesson {
 		return result;
 	}
 
-	public static void main(String[] args) {
+	/* 3. Найти число Фибоначчи под номером N, используя рекурсию. */
+
+	static int fibonachi(int n) {
+
+		if (n == 0) {
+			return 0;
+		}
+		if (n == 1) {
+			return 1;
+		} else {
+			return fibonachi(n - 1) + fibonachi(n - 2);
+		}
+	}
+
 	
+
+	public static void main(String[] args) {
+
 		Scanner scanner = new Scanner(System.in);
-		
-		//1
-		
+
+		// 1
+
 		System.out.println("Введите длинну массива: ");
 		int n = scanner.nextInt();
 		int arr[] = new int[n];
-		randomArray(arr , n);
+		
+		randomArray(arr, n);
 		printArray(arr, n);
 		System.out.println();
-		
+
 		// 2
 
 		System.out.println();
 		System.out.println("Введите число: ");
 		int num = scanner.nextInt();
-		System.out.println("Факториал числа 3 равен " + fact(num));
+		System.out.println("Факториал числа равен " + fact(num));
+		System.out.println();
+
+		// 3
+
+		System.out.println("Введите число: ");
+		int nFib = scanner.nextInt();
+		System.out.println("Число Фибоначчи под номером " + nFib + " равно " + fibonachi(nFib));
+
 		scanner.close();
+
 	}
 
 }
