341bc5d9267b895eda48208fa07e92385b346ca8
Sundarina <sundarina@ukr.net>
2016-11-05 21:28:26 +0200

    построена иерархия классов, проработані методі в главном классе букет.

--- Bouquet.java
+++ Bouquet.java
@@ -1,12 +1,87 @@
-import Flowers.LiveFlowers;
+
+/*1. Цветочница. Определить иерархию цветов. Создать
+несколько объектов-цветов. Собрать букет (используя
+аксессуары) с определением его стоимости. Провести
+сортировку цветов в букете на основе уровня свежести. Найти
+цветок в букете, соответствующий заданному диапазону длин
+стеблей. Создать букет из цветов трех видов, где выбор каждого
+цветка букета происходит случайно. Первый выбирается с
+вероятностью 30 %, второй – 5 %, третий – 65 %.*/
 
 public class Bouquet {
 
-	/*
-	 * bouquet flowers accessories live artificial искуственные price freshness
-	 * свежесть material length sorting the price of the bouquet цена букета
-	 * roses chamomile ромашка thorns шипы the number of petals колич лепестков
-	 */
+	double price;
+	int flowerLength;
+	int freshness;
+	boolean thorns;
+	String material;
+	int min;
+	int max;
+	int n;
+
+	Flowers[] flowers;
+	Accessories[] accesories;
+	int flowersCount;
+	int accesoriesCount;
+	int flowerIndex = 0;
+	int accesoriesIndex = 0;
+
+	public Bouquet(int flowersCount, int accesoriesCount) {
+		this.flowersCount = flowersCount;
+		this.accesoriesCount = accesoriesCount;
+
+		this.flowers = new Flowers[flowersCount];
+		this.accesories = new Accessories[accesoriesCount];
+
+	}
+
+	public Bouquet() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean addFlower(Flowers f) {
+		if (flowerIndex < flowersCount) {
+			this.flowers[flowerIndex] = f;
+			flowerIndex++;
+			return true;
+		}
+		return false;
+	}
+
+	public boolean addAccessories(Accessories a) {
+		if (accesoriesIndex < accesoriesCount) {
+			this.accesories[accesoriesIndex] = a;
+			accesoriesIndex++;
+			return true;
+		}
+		return false;
+	}
+
+	public double bouquetPrice() {
+		double sum = 0;
+
+		for (int i = 0; i < flowers.length; i++) {
+			sum += flowers[i].getPrice();
+		}
+		
+		for (int i = 0; i < accesories.length; i++){
+			sum += accesories[i].getPrice(); 
+		}
+		return sum;
+	}
+
+	public void freshSort(Bouquet[] bouquet, int freshness) {
+
+	}
+
+	public Flowers lengthDiapason(int min, int max) {
+		for (int i = 0; i < flowers.length; i++ ) {
+			if (flowers[i].flowerLength >= min && flowers[i].flowerLength <= max){
+				return flowers[i];
+			} 
+		} 
+		return null;
+	}
 
 	public static void main(String[] args) {
 
@@ -14,17 +89,17 @@ public class Bouquet {
 
 }
 
-class Acccessories {
+class Accessories {
 
 	private double price;
-	public String acccessoriesName;
+	public String accessoriesName;
 
-	public Acccessories(int price, String acccessoriesName) {
+	public Accessories(int price, String accessoriesName) {
 		this.price = price;
-		this.acccessoriesName = acccessoriesName;
+		this.accessoriesName = accessoriesName;
 	}
 
-	public Acccessories(int price) {
+	public Accessories(int price) {
 		this.price = price;
 	}
 
@@ -66,58 +141,82 @@ class Flowers {
 			this.price = -1;
 		}
 	}
+}
 
 class LiveFlowers extends Flowers {
 
-		private int freshness;
-		String grade;
+	protected int freshness;
+	String grade;
 
-		public LiveFlowers(int price, int flowerLength, int freshness, String grade) {
-			super(price, flowerLength);
+	public LiveFlowers(int price, int flowerLength, int freshness, String grade) {
+		super(price, flowerLength);
+		this.freshness = freshness;
+		this.grade = grade;
+	}
+
+	public LiveFlowers(int price, int flowerLength, int freshness) {
+		super(price, flowerLength);
+		this.freshness = freshness;
+	}
+
+	public LiveFlowers() {
+		super();
+		// this.freshness = freshness;
+		// this.grade = grade;
+	}
+
+	public int getFreshness() {
+		return freshness;
+	}
+
+	public void setFreshness(int freshness) {
+		if (freshness >= 1 && freshness <= 10) {
 			this.freshness = freshness;
-			this.grade = grade;
 		}
-
-		public LiveFlowers(int price, int flowerLength, int freshness) {
-			super(price, flowerLength);
-			this.freshness = freshness;
-		}
-
-		public int getFreshness() {
-			return freshness;
-		}
-
-		public void setFreshness(int freshness) {
-			if (freshness >= 1 && freshness <= 10) {
-				this.freshness = freshness;
-			}
-			this.freshness = -1;
-		}
+		this.freshness = -1;
 	}
 }
 
 class ArtificialFlowers extends Flowers {
 
-	private static int freshness = 0;
+	static final int freshnessArtificial = 0;
 	String material;
 
-	public ArtificialFlowers(int price, int flowerLength, int freshness, String material) {
+	public ArtificialFlowers(int price, int flowerLength, int freshnessArtificial, String material) {
 		super(price, flowerLength);
 		this.material = material;
 	}
 
-	public ArtificialFlowers(int price, int flowerLength, int freshness) {
+	public ArtificialFlowers(int price, int flowerLength, int freshnessArtificial) {
 		super(price, flowerLength);
+
 	}
 }
 
 class Roses extends LiveFlowers {
-	
+
 	boolean thorns;
-	
+
 	public Roses(int price, int flowerLength, int freshness, boolean thorns) {
 		super(price, flowerLength, freshness);
 		this.thorns = thorns;
 	}
+
+	public Roses() {
+		super();
+	}
 }
-	
+
+class Chamomile extends LiveFlowers {
+
+	int petalsNum;
+
+	public Chamomile(int price, int flowerLength, int freshness, String grade, int petalsNum) {
+		super(price, flowerLength, freshness, grade);
+		this.petalsNum = petalsNum;
+	}
+
+	public Chamomile() {
+		super();
+	}
+}
